a1ea7bd8f9cae77ac824b78654288bd7380113a8
domederu2-commits <domederu2@gmail.com>
2025-11-06 19:29:17 +0100

    Esercizio JavaScript - Traccia 1

---
+++ project.js
@@ -0,0 +1,17 @@
+// Dichiarazione delle variabili e assegnazione dei valori
+let a = 10;
+let b = 20;
+let c = 30;
+let d = 40;
+let e = 50;
+
+// Calcolo della somma
+let sum = a + b + c + d + e;
+
+// Calcolo della media
+let media = sum / 5;
+
+// Stampa dei risultati in console
+console.log("La somma è: " + sum);
+console.log("La media è: " + media);
+
